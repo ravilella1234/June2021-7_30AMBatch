9b4d101adbd4aeea5d2e17f3f030bdd513967b3f
maha <maha@gmail.com>
2021-07-15 09:02:26 +0530

    Browser User define Profiles

--- July7_30AMSeleniumBatch/src/com/Launchings/BaseTest.java
+++ July7_30AMSeleniumBatch/src/com/Launchings/BaseTest.java
@@ -5,7 +5,12 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -43,15 +48,54 @@ public class BaseTest
 		{
 			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\July Browser Drivers\\chromedriver.exe");
 			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+			System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "logs//chromelog.log");
+			
+			ChromeOptions option = new ChromeOptions();
+			option.addArguments("user-data-dir=C:\\Users\\DELL\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 23");
+			option.addArguments("--disable-notifications");
+			option.addArguments("--start-maximized");
+			option.addArguments("--ignore-certificate-errors");
+			//option.addArguments("--proxy-server=http://192.168.10.1:9090");
+			
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(option);
 		}
 		else if(p.getProperty(browser).equals("firefox"))
 		{
 			//System.setProperty("webdriver.gecko.driver", "C:/Users/DELL/Desktop/July Browser Drivers/geckodriver.exe");
 			//System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+			
+			
+			//Binaries
+			//logs
+			//Notifications
+			//Certificate errors
+			//work with proxy settings
+			
 			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
+			ProfilesIni p = new ProfilesIni();
+			FirefoxProfile profile = p.getProfile("julyffprofile");
+			
+			FirefoxOptions option = new FirefoxOptions();
+			option.setProfile(profile);
+			
+			//Binaries
+			option.setBinary("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+			System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "logs\\firefoxlogs.log");
+			
+			//Notifications
+			profile.setPreference("dom.webnotifications.enabled", false);
+				
+			//Certificate errors
+			profile.setAcceptUntrustedCertificates(true);
+			profile.setAssumeUntrustedCertificateIssuer(false);
+			
+			//work with proxy settings
+			profile.setPreference("network.proxy.type", 1);
+			profile.setPreference("network.proxy.socks;", "192.156.10.1");
+			profile.setPreference("network.proxy.socks_port;0", 1765);
+			
+			driver = new FirefoxDriver(option);
 		}
 	}
 	
---
+++ July7_30AMSeleniumBatch/src/com/Launchings/TC_003.java
@@ -0,0 +1,15 @@
+package com.Launchings;
+
+public class TC_003 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		launch("chromebrowser");
+		
+		navigate("certificateurl");
+	}
+
+}
